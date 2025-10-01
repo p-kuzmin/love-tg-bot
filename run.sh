30c7d4971419e62771b76df76a34cd65dd441541
Pavel Kuzmin <paultrkuz@gmail.com>
2025-10-02 03:37:43 +0500

    add: run bash script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+if [ "$(docker ps -aq -f name=daily-tg-bot)" ]; then
+    echo "Removing existing container: daily-tg-bot"
+    docker rm -f daily-tg-bot
+fi
+
+echo "Building Docker image"
+docker build -t daily-tg-bot . --no-cache
+
+echo "Running container"
+docker run --name daily-tg-bot --env-file .env daily-tg-bot

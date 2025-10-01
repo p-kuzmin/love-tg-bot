ca8c0926deb7b55f3bd3ec384d1e76e551033354
Pavel Kuzmin <paultrkuz@gmail.com>
2025-10-02 03:56:20 +0500

    add: remove old image when rebuild the container

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-if [ "$(docker ps -aq -f name=daily-tg-bot)" ]; then
-    echo "Removing existing container: daily-tg-bot"
-    docker rm -f daily-tg-bot
+IMAGE_NAME="daily-tg-bot"
+CONTAINER_NAME="daily-tg-bot"
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    echo "Removing existing container: $CONTAINER_NAME"
+    docker rm -f $CONTAINER_NAME
+fi
+
+if [ "$(docker images -q $IMAGE_NAME)" ]; then
+    echo "Removing existing image: $IMAGE_NAME"
+    docker rmi -f $IMAGE_NAME
 fi
 
 echo "Building Docker image"
-docker build -t daily-tg-bot . --no-cache
+docker build -t $IMAGE_NAME . --no-cache
 
-echo "Running container"
-docker run --name daily-tg-bot --env-file .env daily-tg-bot
+echo "Running container in detached mode"
+docker run -d --name $CONTAINER_NAME --env-file .env $IMAGE_NAME
+

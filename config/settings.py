c6a10edaee4116b6486eaea44bbce69837cc1f8a
Pavel Kuzmin <paultrkuz@gmail.com>
2025-12-21 01:12:33 +0500

    chore: some change to import env

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-ROUTER_MODEL = os.getenv("OPENROUTER_MODEL")
+ROUTER_API_KEY = os.getenv("ROUTER_API_KEY")
+ROUTER_MODEL = os.getenv("ROUTER_MODEL")
 TG_TOKEN = os.getenv("TG_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
